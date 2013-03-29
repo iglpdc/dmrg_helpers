21ce339bf720de7a6e54996b3a16a1f4daee3a9d
Ivan Gonzalez <iglpdc@gmail.com>
2013-03-29 17:42:24 -0400

    Adding a couple of functions.

--- dmrg_helpers/extract/estimator.py
+++ dmrg_helpers/extract/estimator.py
@@ -1,6 +1,7 @@
 '''A class to store estimators once retrieved from the database.
 '''
 import numpy as np
+from itertools import izip
 from dmrg_helpers.extract.estimator_site import EstimatorSite
 
 class EstimatorData(object):
@@ -51,6 +52,9 @@ class Estimator(object):
         The keys from the metadata dictionary joined by the ':' delimiter. 
         The keys are alphabetically ordered. It stores the metadata of the
         estimator, like the names of the parameters of the Hamiltonian.
+    keys: a tuple of strings.
+        Obtained from meta_keys. Used to inspect which are the keys, like
+        parameters of the Hamiltonian that label your data.
     data: a dict of a string on EstimatorData.
         Contains the actual values for the estimator. The key in the dictionary 
         is given by the parameters that characterize the data, such as 
@@ -60,14 +64,25 @@ class Estimator(object):
     def __init__(self, name, meta_keys):
         self.name = name
         self.meta_keys = meta_keys
+        self.keys = self.meta_keys.split(':')
         self.data = {}
 
     def __len__(self):
         """Returns number of entry of the data dict.
 
-        Mostly for testing purposes.
+        You use this function to inspect which are the keys, like parameters of 
+        the Hamiltonain that label your data.
         """
         return len(self.data)
+
+    def get_metadata_as_dict(self, meta_val):
+        """Returns a dictionary with metadata.
+        
+        Parameters
+        ----------
+        meta_val: one of the meta_vals.
+        """
+        return dict(izip(self.keys, meta_val.split(':'))) 
 
     def add_fetched_data(self, fetched_data):
         """Adds data fecthed from the database to the Estimator.
@@ -88,14 +103,47 @@ class Estimator(object):
     def save(self, filename):
         """Saves the correlator data to a file.
 
+        You use this function to save data for a correlator to a file. If there 
+        is more that one set of data in the Correlator, for example, becuase 
+        you have data for different systems sizes, each set will be saved into
+        a different file. The name of these files will be obtained by appending 
+        the names and values of the meta_data to `filename`.
+
+        Inside the file the data is organized in two columns: the first is a 
+        site of the chain, and the second the value of the correlator.
+        """
+        for fn in self.generate_filenames(filename):
+            tmp = izip(self.data[fn].x(), self.data[fn].y())
+            with open(fn, 'w') as f:
+                f.write('\n'.join('%s %s' % x for x in tmp))
+
+    def generate_filenames(self, filename):
+        """Generates one filename per entry in data according to `label`.
+
         Parameters
         ----------
         filename: a string.
             The filename to be created.
-        """
-        pass
 
-    def plot(self, label = None):
+        Returns
+        -------
+        filenames: a list of strings.
+            The result is to append the labels with their values to `filename`.
+
+        Raises
+        ------
+        DMRGException if the label is not found in self.keys.
+        """
+        filenames = []
+        for meta_val in self.data.keys():
+            meta_dict = self.get_metadata_as_dict(meta_val)
+            extended_filename = filename
+            for key, val in meta_dict.values():
+                extended_filename += '_'+str(key)+'_'+str(val)
+            filenames.append(extended_filename)
+        return dict(izip(self.data.keys(), filenames))
+
+    def plot(self, label=None):
         """Plots the data.
 
         Makes a plot of the correlator data. If the correlator contains several
