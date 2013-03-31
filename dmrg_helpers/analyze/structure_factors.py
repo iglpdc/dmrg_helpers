059362e072e9aa9f21be75d645ccfd766b2c2ef6
Ivan Gonzalez <iglpdc@gmail.com>
2013-03-31 10:49:56 -0400

    Adding some basic function here.

--- dmrg_helpers/analyze/structure_factors.py
+++ dmrg_helpers/analyze/structure_factors.py
@@ -1,18 +1,34 @@
 '''
 Functions to calculate common structure factors.
 '''
-from dmrg_helpers.analyze.correlator import Correlator
+from dmrg_helpers.extract.estimator import Estimator
+from itertools import izip
 
-def calculate_spin_struct_factor(database):
+def calculate_spin_struct_factor(db):
     """Calculates the (full) spin structure factor.
     
     Parameters
     ----------
-    database: a Database object.
+    db: a Database object.
         The database obtained after reading the estimators.dat files.
 
     Returns
     -------
-    A Correlator object with the spin structure factor.
+    A Estimator object with the spin structure factor.
+
+    Example
+    -------
+    >>> from dmrg_helpers.analyze.structure_factors import (
+    ...     calculate_spin_struct_factor)
+    >>> from dmrg_helpers.extract.database import Database
+    >>> db = Database() # just empty for the example
+    >>> spin_struct_factor = calculate_spin_struct_factor(db)
+    >>> spin_struct_factor.save('spin_struct_factor.dat', 'tests')
     """
-    return Correlator
+    zz_component = db.get_estimator('S_z*S_z')
+
+    meta_keys = zz_component.meta_keys
+    result = Estimator('spin_struct_factor', meta_keys)
+    result.data = dict(izip(zz_component.x(), zz_component.y()))
+
+    return result 
