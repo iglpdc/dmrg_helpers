3bd381955f0d56968eed1047cdee0202734761f3
Ivan Gonzalez <iglpdc@gmail.com>
2013-03-31 15:54:14 -0400

    Fixed example.

--- dmrg_helpers/analyze/structure_factors.py
+++ dmrg_helpers/analyze/structure_factors.py
@@ -2,7 +2,6 @@
 Functions to calculate common structure factors.
 '''
 from dmrg_helpers.extract.estimator import Estimator
-from itertools import izip
 
 def calculate_spin_struct_factor(db):
     """Calculates the (full) spin structure factor.
@@ -20,15 +19,19 @@ def calculate_spin_struct_factor(db):
     -------
     >>> from dmrg_helpers.analyze.structure_factors import (
     ...     calculate_spin_struct_factor)
-    >>> from dmrg_helpers.extract.database import Database
-    >>> db = Database() # just empty for the example
+    >>> from dmrg_helpers.extract.extract import create_db_from_file
+    >>> db = create_db_from_file('tests/real_data/static/estimators.dat',
+    ...                          'tests/real_data/real_data.sqlite')
     >>> spin_struct_factor = calculate_spin_struct_factor(db)
     >>> spin_struct_factor.save('spin_struct_factor.dat', 'tests')
+    >>> # delete the database file
+    >>> import os
+    >>> os.remove('tests/real_data/real_data.sqlite')
     """
-    zz_component = db.get_estimator('S_z*S_z')
+    zz_component = db.get_estimator('s_z*s_z')
 
     meta_keys = zz_component.meta_keys
     result = Estimator('spin_struct_factor', meta_keys)
-    result.data = dict(izip(zz_component.x(), zz_component.y()))
+    #result.data = dict(izip(zz_component.data.x(), zz_component.data.y()))
 
     return result 
