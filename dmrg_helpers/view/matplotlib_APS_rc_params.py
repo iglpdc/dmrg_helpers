ef44816a79052e47c9f5e0f7718a316914fcf664
Ivan Gonzalez <iglpdc@gmail.com>
2013-04-03 15:15:27 -0400

    Fixes here.

--- dmrg_helpers/view/matplotlib_APS_rc_params.py
+++ dmrg_helpers/view/matplotlib_APS_rc_params.py
@@ -9,19 +9,17 @@ Modified from:
 Examples
 --------
 >>> import pylab
->>> from pylab import arange,pi,sin,cos
+>>> from pylab import arange, pi, sin, cos
 >>> import dmrg_helpers.view.matplotlib_APS_rc_params as aps_params
->>> pylab.rcParams.update(aps_params.aps)
+>>> pylab.rcParams.update(aps_params.aps['params'])
 >>> # Generate data
->>> x = pylab.arange(-2*pi,2*pi,0.01)
->>> y1 = sin(x)
+>>> x = pylab.arange(-2*pi, 2*pi, 0.01)
+>>> y = sin(x)
 >>> # Plot data
->>> pylab.figure(1)
->>> pylab.clf()
->>> pylab.axes([0.125,0.2,0.95-0.125,0.95-0.2])
->>> pylab.plot(x,y1,'g:',label='$\sin(x)$')
->>> pylab.xlabel('$x$ (radians)')
->>> pylab.ylabel('$y$')
+>>> pylab.figure(1) #doctest: +ELLIPSIS
+<...
+>>> pylab.axes(aps_params.aps['axes']) #doctest: +ELLIPSIS
+<...
 """
 # documentclass 'article' with package 'fullpage'
 fullpage = {'params': {'axes.labelsize': 10,
@@ -32,7 +30,7 @@ fullpage = {'params': {'axes.labelsize': 10,
                        'text.usetex': True,
                        'font.family': 'serif',
                        'figure.figsize': (4.774, 2.950)},
-            'axes': [0.150,0.175,0.95-0.15,0.95-0.25]}
+            'axes': [0.150, 0.175, 0.95-0.15, 0.95-0.25]}
 
 # two-column APS journal format
 aps = {'params': {'backend': 'ps',
@@ -43,4 +41,4 @@ aps = {'params': {'backend': 'ps',
                   'ytick.labelsize': 8,
                   'text.usetex': True,
                   'figure.figsize': (3.4039, 2.1037)},
-       'axes': [0.125,0.2,0.95-0.125,0.95-0.2]}
+       'axes': [0.125, 0.2, 0.95-0.125, 0.95-0.2]}
