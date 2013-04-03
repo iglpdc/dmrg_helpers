19ee468557e6ec0a05af820d2e3e20893d08387e
Ivan Gonzalez <iglpdc@gmail.com>
2013-04-03 13:20:16 -0400

    Both spin and charge structure factors work fine. Diagonal elements are not addressed yet.

--- dmrg_helpers/analyze/fourier.py
+++ dmrg_helpers/analyze/fourier.py
@@ -2,8 +2,9 @@
 '''
 import numpy as np
 from math import pi
-from itertools import izip
+from itertools import izip, chain
 from dmrg_helpers.view.xy_data import XYData, XYDataDict
+from dmrg_helpers.core.dmrg_exceptions import DMRGException
 
 def calculate_fourier_comp_for_two_point_estimator(estimator_data, q):
     """Calculates the Fourier transform for an EstimatorData object.
@@ -22,7 +23,6 @@ def calculate_fourier_comp_for_two_point_estimator(estimator_data, q):
     sites = map(list, zip(*estimator_data.sites())) 
     diff = np.array(sites[0], dtype=int)-np.array(sites[1], dtype=int)
     return 2*np.sum(np.multiply(estimator_data.y_as_np(), np.cos(q*diff)))
-    #return np.sum(np.multiply(estimator_data.y_as_np(), np.exp(1j*q*diff)).real)
 
 def generate_momenta(length):
     """Generates the allowed momenta for a system of a given `length`.
@@ -86,8 +86,35 @@ def calculate_fourier_transform_for_two_point_estimator(estimator,
             calculate_fourier_transform_for_two_point_estimator_data(data,
                                                                      length))
     return XYDataDict(estimator.meta_keys, 
-                      dict(izip(estimator.data.iterkeys(), fourier_transforms)))
+                      dict(izip(estimator.data.iterkeys(), 
+                           fourier_transforms)))
 
 def get_length_directly_from_data(estimator_data):
-    return int(96)
+    """Calculates the length of the system from the estimator data.
 
+    You use this function to cover up for missing information in your data. In
+    case you don't have other way to find the length of the DMRG chainm you can
+    always do this: assume that the length is the distance between the smallest
+    and largest indexes that show up in the estimator sites.
+
+    Note that this is not fine, if for some reason you decide to exclude
+    certain parts of the chain from the measurement. 
+
+    Parameters
+    ----------
+    estimator_data: a EstimatorData object.
+        The data you are using.
+
+    Returns
+    -------
+    length: an int.
+
+    Raises
+    ------
+    DMRGException if estimator_data is empty.
+
+    """
+    if not estimator_data.sites():
+        raise DMRGException('Empty estimator data')
+    tmp = [int(i) for i in chain.from_iterable(estimator_data.sites())]
+    return max(tmp)-min(tmp)+1
--- dmrg_helpers/analyze/structure_factors.py
+++ dmrg_helpers/analyze/structure_factors.py
@@ -1,11 +1,12 @@
 '''
 Functions to calculate common structure factors.
 '''
+import numpy as np
 from dmrg_helpers.analyze.fourier import (
     calculate_fourier_transform_for_two_point_estimator)
 
 def calculate_spin_struct_factor(db):
-    """Calculates the (full) spin structure factor.
+    """Calculates the (longitudinal) spin structure factor.
     
     Parameters
     ----------
@@ -21,16 +22,46 @@ def calculate_spin_struct_factor(db):
     >>> from dmrg_helpers.analyze.structure_factors import (
     ...     calculate_spin_struct_factor)
     >>> from dmrg_helpers.extract.extract import create_db_from_file
-    >>> db = create_db_from_file('tests/real_data/static/estimators.dat',
-    ...                          'tests/real_data/real_data.sqlite')
+    >>> db = create_db_from_file('tests/real_data/static/estimators.dat')
     >>> spin_struct_factor = calculate_spin_struct_factor(db)
     >>> spin_struct_factor.save('spin_struct_factor.dat', 'tests')
-    >>> # delete the database file
-    >>> import os
-    >>> os.remove('tests/real_data/real_data.sqlite')
     """
     zz_component = db.get_estimator('s_z*s_z')
     result = ( 
         calculate_fourier_transform_for_two_point_estimator(zz_component, 
+                                                            'number_of_sites'))
+    return result 
+
+def calculate_density_struct_factor(db):
+    """Calculates the density (charge) structure factor.
+    
+    Parameters
+    ----------
+    db: a Database object.
+        The database obtained after reading the estimators.dat files.
+
+    Returns
+    -------
+    A Estimator object with the charge structure factor.
+
+    Example
+    -------
+    >>> from dmrg_helpers.analyze.structure_factors import (
+    ...     calculate_density_struct_factor)
+    >>> from dmrg_helpers.extract.extract import create_db_from_file
+    >>> db = create_db_from_file('tests/real_data/static/estimators.dat')
+    >>> charge_struct_factor = calculate_density_struct_factor(db)
+    >>> charge_struct_factor.save('charge_struct_factor.dat', 'tests')
+    """
+    fluctuations = db.get_estimator('n*n')
+    n = db.get_estimator('n')
+    for key, val in fluctuations.data.iteritems():
+        n_vals = n.data[key].y_as_np()
+        tmp = -1.0 * np.array([n_vals[int(s[0])] * n_vals[int(s[1])] 
+                              for s in val.sites()])
+        tmp += val.y_as_np()
+        val.values_list = tmp.tolist()
+    result = ( 
+        calculate_fourier_transform_for_two_point_estimator(fluctuations, 
                                                             'number_of_sites'))
     return result 
